de4419cf63c7c5fbfbe68756f985b9cc7bef1d93
Wade Wegner <wade.wegner@gmail.com>
2015-01-06 22:50:33 -0700

    bad echo

--- deploy.cmd
+++ deploy.cmd
@@ -11,7 +11,7 @@ set hr=%time:~0,2%
 if "%hr:~0,1%" equ " " set hr=0%hr:~1,1%
 SET FOLDER=%date:~-4,4%%date:~-10,2%%date:~-7,2%_%hr%%time:~3,2%%time:~6,2%
 
-ECHO FOLDER
+ECHO %FOLDER%
 
 rem MKDIR %NEWHOME%\site\wwwroot\gopath\src\%FOLDER%
 
