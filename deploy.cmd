00e6a63a448e792eac542463e6b466f100acc7c9
Wade Wegner <wade.wegner@gmail.com>
2015-01-07 00:02:53 -0700

    Using PS to kill process

--- deploy.cmd
+++ deploy.cmd
@@ -9,8 +9,6 @@ set hr=%time:~0,2%
 if "%hr:~0,1%" equ " " set hr=0%hr:~1,1%
 SET FOLDER=%date:~-4,4%%date:~-10,2%%date:~-7,2%_%hr%%time:~3,2%%time:~6,2%
 
-ECHO %FOLDER%
-
 MKDIR %NEWHOME%\site\wwwroot\gopath\src\%FOLDER%
 
 xcopy %DEPLOYMENT_SOURCE% %NEWHOME%\site\wwwroot\gopath\src\%FOLDER% /Y
@@ -20,10 +18,8 @@ SET GOPATH=%NEWHOME%\site\wwwroot\gopath
 SET GOROOT=%NEWHOME%\site\wwwroot\go
 SET PATH=%PATH%;%GOPATH%\bin;%NEWHOME%\site\wwwroot\go\bin
 
-ECHO %PATH%
-
 go get %FOLDER%
 
 sed -i 's/GOAPPBINARY/%FOLDER%.exe/g' %NEWHOME%\site\wwwroot\Web.Config
 
-%DEPLOYMENT_SOURCE%\tlist.exe -t
+powershell "stop-process (Get-Process w3wp | Sort-Object ws | Select -first 1).Id"
